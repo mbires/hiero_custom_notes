e38300e11dad5d8c05dc48ec3f3c9e77c2231e28
mbires <marek0271@gmail.com>
2025-12-02 07:25:17 +0100

    Refactor gizmo loading and custom notes registration

--- register_hiero_custom_notes.py
+++ register_hiero_custom_notes.py
@@ -15,17 +15,16 @@ gizmo_path = os.path.dirname(__file__)
 nuke.pluginAddPath(gizmo_path)
 
 try:
-    nuke.load("{}/{}".format(gizmo_path, 'MB_hiero_custom_notes.gizmo'))
+    # This creates an action with an icon and effect
+    custom_notes = QAction(QIcon("icons:LUT.png"), "MB Custom Notes", None)
+
+    # Soft effect actions can be found by prefixing the QAction's objectName with: 'foundry.timeline.effect'
+    custom_notes.setObjectName("foundry.timeline.effect.CustomNotes")
+    custom_notes.setData('MB_hiero_custom_notes')
+
+    # This registers your custom action with the Effects Menu
+    registerAction(custom_notes)
+
     print("Gizmo 'MB_hiero_custom_notes' loaded")
 except Exception as e:
     print(f"Failed to load gizmo MB_hiero_custom_notes: {e}")
-
-# This creates an action with an icon and effect
-custom_notes = QAction(QIcon("icons:LUT.png"), "MB Custom Notes", None)
-
-# Soft effect actions can be found by prefixing the QAction's objectName with: 'foundry.timeline.effect'
-custom_notes.setObjectName("foundry.timeline.effect.CustomNotes")
-custom_notes.setData('MB_hiero_custom_notes')
-
-# This registers your custom action with the Effects Menu
-registerAction(custom_notes)
